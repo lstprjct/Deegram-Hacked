ea7c8b89ad74ad564cd5241c58d9937489f266aa
S M Mahabub Hossain <77372461+priiiiyo@users.noreply.github.com>
2022-08-05 14:33:24 +0600

    Update script
    Signed-off-by: S M Mahabub Hossain <77372461+priiiiyo@users.noreply.github.com>

--- update.py
+++ update.py
@@ -1,53 +1,62 @@
-import os
-import subprocess
-import requests
-import logging
-
+from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
+from os import path as ospath, environ, execl as osexecl
+from subprocess import run as srun
+from requests import get as rget
 from dotenv import load_dotenv
+from sys import executable
 
-if os.path.exists('log.txt'):
+if ospath.exists('log.txt'):
     with open('log.txt', 'r+') as f:
         f.truncate(0)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
-                    level=logging.INFO)
+basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[FileHandler('log.txt'), StreamHandler()],
+                    level=INFO)
 
-CONFIG_FILE_URL = os.environ.get('CONFIG_FILE_URL', None)
+CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
 try:
     if len(CONFIG_FILE_URL) == 0:
         raise TypeError
     try:
-        res = requests.get(CONFIG_FILE_URL)
+        res = rget(CONFIG_FILE_URL)
         if res.status_code == 200:
             with open('config.env', 'wb+') as f:
                 f.write(res.content)
-                f.close()
         else:
-            logging.error(f"Failed to download config.env {res.status_code}")
+            log_error(f"Failed to download config.env {res.status_code}")
     except Exception as e:
-        logging.error(str(e))
-except TypeError:
+        log_error(f"CONFIG_FILE_URL: {e}")
+except:
     pass
 
 load_dotenv('config.env', override=True)
 
-UPSTREAM_REPO = os.environ.get('UPSTREAM_REPO', None)
+UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
 try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
-except TypeError:
-    UPSTREAM_REPO = "https://github.com/appeza/mirror-leech-bot"
+except:
+    UPSTREAM_REPO = "https://github.com/priiiiyo/tg-mirror-leech-bot"
+try:
+    if len(UPSTREAM_BRANCH) == 0:
+       raise TypeError
+except:
+    UPSTREAM_BRANCH = 'update'
 
-if os.path.exists('.git'):
-    subprocess.run(["rm", "-rf", ".git"])
+if ospath.exists('.git'):
+    srun(["rm", "-rf", ".git"])
 
-subprocess.run([f"git init -q \
-                  && git config --global user.email priiiiyoplus@gmail.com \
-                  && git config --global user.name mlb \
-                  && git add . \
-                  && git commit -sm update -q \
-                  && git remote add origin {UPSTREAM_REPO} \
-                  && git fetch origin -q \
-                  && git reset --hard origin/master -q"], shell=True)
+update = srun([f"git init -q \
+                 && git config --global user.email priiiiyoplus@gmail.com \
+                 && git config --global user.name priiiiyo \
+                 && git add . \
+                 && git commit -sm update -q \
+                 && git remote add origin {UPSTREAM_REPO} \
+                 && git fetch origin -q \
+                 && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
+if update.returncode == 0:
+    log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+else:
+    log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
